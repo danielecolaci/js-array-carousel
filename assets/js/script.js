506988ed51e9b488fe28557e9e76212de865a2ba
danielecolaci <danielecolaci@outlook.com>
2024-02-23 20:28:43 +0100

    Add Next function

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,19 +7,55 @@ const content = [
     '05.webp',
 ];
 
-const activeImage = 0;
+let activeImage = 0;
 const contentElement = document.querySelector('.content');
+
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
 
 /* console.log(content, activeImage, contentElement); */
 
 for (let index = 0; index < content.length; index++) {
     const slide = content[index];
-    console.log(slide);
+    /* console.log(slide); */
 
-    const slideMarkup = `<img class=${index === activeImage ? 'active' : ''} src="./assets/img/${slide}" alt="">`
-    console.log(slideMarkup);
+    const slideMarkup = `<img class="${index === activeImage ? 'active' : ''}" src="./assets/img/${slide}" alt="">`
+    /* console.log(slideMarkup); */
 
     contentElement.insertAdjacentHTML('beforeend', slideMarkup);
 
-
 }
+
+//Tasti Prev e Next
+
+console.log(prevButton, nextButton);
+
+prevButton.addEventListener('click', function () {
+    console.log('slidePrev');
+
+    activeImage--
+    console.log(activeImage);
+
+})
+
+// Next
+nextButton.addEventListener('click', function () {
+    console.log('slideNext');
+
+    activeImage++
+    console.log(activeImage);
+
+    const currentImage = document.querySelector('img.active');
+    console.log(currentImage);
+    currentImage.classList.remove('active');
+
+    const allImage = document.querySelectorAll('.content img');
+
+    console.log((activeImage, content, allImage));
+    console.log(allImage[activeImage]);
+
+    allImage[activeImage].classList.add('active');
+
+})
+
+
