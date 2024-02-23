1262e05ddc03d51289cea976e84674ae633312a5
danielecolaci <danielecolaci@outlook.com>
2024-02-23 18:44:44 +0100

    Add dynamic images

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-console.log('ok');
+//Array di immagini
+const content = [
+    '01.webp',
+    '02.webp',
+    '03.webp',
+    '04.webp',
+    '05.webp',
+];
+
+const activeImage = 0;
+const contentElement = document.querySelector('.content');
+
+/* console.log(content, activeImage, contentElement); */
+
+for (let index = 0; index < content.length; index++) {
+    const slide = content[index];
+    console.log(slide);
+
+    const slideMarkup = `<img class=${index === activeImage ? 'active' : ''} src="./assets/img/${slide}" alt="">`
+    console.log(slideMarkup);
+
+    contentElement.insertAdjacentHTML('beforeend', slideMarkup);
+
+
+}
